098a07e7787ee2d8369d1bc8c84cf0f9bea7e517
tomoumer <tomo.umer@gmail.com>
2024-05-15 18:01:04 -0500

    polished the app

--- streamlit/civics_app.py
+++ streamlit/civics_app.py
@@ -17,6 +17,15 @@ if 'stage' not in st.session_state:
 if 'question_count' not in st.session_state:
     st.session_state.question_count = 0
 
+if 'current_question' not in st.session_state:
+    st.session_state.current_question = ''
+
+if 'correct_answers' not in st.session_state:
+    st.session_state.correct_answers = []
+
+if 'your_answers' not in st.session_state:
+    st.session_state.your_answers = []
+
 if 'answer_score' not in st.session_state:
     st.session_state.answer_score = []
 
@@ -35,31 +44,45 @@ def setup_questions(max_q):
     st.session_state.total_score = [0, 0, 0]
     update_stage(1)
 
-def check_answer(your_answers, correct_answers):
-    
+#def check_answer(your_answers, correct_answers):
+def check_answer():
+
     # to construct, it's going to have: your answer, best actual answer and score
     # for each answer (if more than one)
     st.session_state.answer_score = []
 
-    # copy the answers for displaying later
-    st.session_state.your_last_answer = your_answers
+    st.session_state.your_answers.append(st.session_state.chat_in)
 
-    # I'll have to remove elements after matching when the question asks for more than 1 answer
-    tmp_answers = correct_answers.copy()
+    # special state if the question asks for 2 answers, or 3
+    if ('two' in st.session_state.current_question
+        and 'one of the two' not in st.session_state.current_question
+        and st.session_state.stage == 1):
+        update_stage(12)
 
-    # for each of the answers, find the closest match
-    for your_answer in your_answers:
-        tmp_answer_score = [your_answer]
-        # take only the closest match
-        tmp_answer_score.extend(process.extract(your_answer, tmp_answers, scorer=fuzz.token_sort_ratio)[0])
-        # remove from other matching
-        tmp_answers.remove(tmp_answer_score[1])
-        
-        st.session_state.answer_score.append(tmp_answer_score)
-    update_stage(2)
+    # similar but for 3 answers
+    elif 'Name three ' in st.session_state.current_question:
+        if st.session_state.stage == 12:
+            update_stage(13)
+        elif st.session_state.stage == 1:
+            update_stage(12)
+
+    else:
+        # I'll have to remove elements after matching when the question asks for more than 1 answer
+        tmp_answers = st.session_state.correct_answers.copy()
+
+        # for each of the answers, find the closest match
+        for your_answer in st.session_state.your_answers:
+            tmp_answer_score = [your_answer]
+            # take only the closest match
+            tmp_answer_score.extend(process.extract(your_answer, tmp_answers, scorer=fuzz.token_sort_ratio)[0])
+            # remove from other matching
+            tmp_answers.remove(tmp_answer_score[1])
+            
+            st.session_state.answer_score.append(tmp_answer_score)
+        update_stage(2)
 
 
-def update_stage(stage): #cur_q, chosen_q
+def update_stage(stage):
 
     # to get to the end one
     if (st.session_state.stage == 2 
@@ -68,10 +91,18 @@ def update_stage(stage): #cur_q, chosen_q
     else:
         st.session_state.stage = stage
 
-    if stage == 0:
+    if st.session_state.stage == 0:
         st.session_state.question_count = 0
-    elif stage == 1:
+    elif st.session_state.stage == 1:
         st.session_state.question_count += 1
+
+        # get the new question and answers, reset your answers
+        st.session_state.your_answers = []
+        # just a temp variable, to get actual question number
+        q_num = str(st.session_state.chosen_questions[st.session_state.question_count - 1])
+        # current question and answers
+        st.session_state.current_question = list(civics_final[q_num].keys())[0]
+        st.session_state.correct_answers = list(civics_final[q_num].values())[0]
 
     
 
@@ -83,10 +114,12 @@ if st.session_state.stage == 0:
     with st.expander('Show instructions'):
             st.markdown("""
                         - Select number of questions you want to answer and 'Start Quiz'
-                        - You can 'Reset Quiz' at any point for any reason and start again
-                        - Write the answer(s) and press 'Submit'
-                        - The resulting page will show you which of the possible answer(s) most closly matched your answer
-                        - You can also review additional possible answers
+                        - You can 'Reset Quiz' at any point
+                        - Type out your answers and submit
+                            - If a question has multiple answers, there will be a note mentioning it
+                            - submit one answer at a time (each one has to be different)
+                        - The resulting page will show you which of the possible answer(s) most closly matched your answer(s)
+                            - You can also check out and review additional possible answers
                         - Please note that there are a few questions specific to the State and (at least for right now), I decided to fill it with the one where I was so warmly welcomed - 4th congressional district of [Tennessee](https://en.wikipedia.org/wiki/Tennessee), the volunteer state!
                         - Special thanks to [Nashville Software School](https://nashvillesoftwareschool.com) who helped me gain the skills necessary to do this!
             """)
@@ -95,31 +128,37 @@ if st.session_state.stage == 0:
 
 if st.session_state.stage == 1:
     quiz_progress = (st.session_state.question_count-1)/len(st.session_state.chosen_questions)
-    # extract the right question number based on the index
-    q_num = str(st.session_state.chosen_questions[st.session_state.question_count - 1])
-    question = list(civics_final[q_num].keys())[0]
-    correct_answers = list(civics_final[q_num].values())[0]
-    #st.write(question)
+
     #make question fancy in blue
-    st.info(f'{question}')
+    st.info(f'{st.session_state.current_question}')
 
-    your_answers = []
-    your_answers.append(st.text_input('Your answer:', ''))
+    if (('two' in st.session_state.current_question
+        and 'one of the two' not in st.session_state.current_question)
+        or ('Name three ' in st.session_state.current_question)):
+        st.write('NOTE: This question requires multiple answers. Please submit one at a time.')
 
-    if 'two' in question and 'one of the two' not in question:
-        your_answers.append(st.text_input('Second answer:', ''))
-    elif 'Name three ' in question:
-        your_answers.append(st.text_input('Second answer:', ''))
-        your_answers.append(st.text_input('Third answer:', ''))
+    #your_answers.append(st.text_input('Your answer:', ''))
+    with st.container():
+        st.chat_input('Your answer:', on_submit=check_answer, key='chat_in')#, args=(correct_answers,))
 
-    st.button('Submit', on_click=check_answer, args=(your_answers, correct_answers))#, st.session_state.chosen_questions))
-    
+    #st.button('Submit', on_click=check_answer, args=(your_answers, correct_answers))#, st.session_state.chosen_questions))
+
+# the stages 12 and 13 are for second and 3rd question
+if st.session_state.stage in (12, 13):
+    quiz_progress = (st.session_state.question_count-1)/len(st.session_state.chosen_questions)
+
+    st.info(f'{st.session_state.current_question}')
+
+    for i, answer in enumerate(st.session_state.your_answers):
+        st.write(f'Your Answer {i+1}:', answer)
+
+    with st.container():
+        st.chat_input('Your answer:', on_submit=check_answer, key='chat_in')
+
+
 if st.session_state.stage == 2:
     quiz_progress = st.session_state.question_count/len(st.session_state.chosen_questions)
     #st.write('current q', st.session_state.question_count)
-    q_num = str(st.session_state.chosen_questions[st.session_state.question_count - 1])
-    question = list(civics_final[q_num].keys())[0]
-    correct_answers = list(civics_final[q_num].values())[0]
 
     printout = '| Question    | Your Answer | Correct Answer |\n| ----------- | ----------- | -------------- |\n'
     tmp_score = 0
@@ -127,7 +166,7 @@ if st.session_state.stage == 2:
     # there will be 1 on more depending on how many answers
     for i, answer_score in enumerate(st.session_state.answer_score):
         # note question is the same here, might have multiple answers
-        printout += f'| {question} | {answer_score[0]} | {answer_score[1]} |\n'
+        printout += f'| {st.session_state.current_question} | {answer_score[0]} | {answer_score[1]} |\n'
         tmp_score += answer_score[2]
 
     # to get the avg. and then turn it to int
@@ -150,7 +189,6 @@ if st.session_state.stage == 2:
 
     #st.write(st.session_state.answer_score)
 
-
     # st.write('- Question:', question)
     # st.write('- Your Answer:', st.session_state.your_last_answer[0])
     # st.write('- Correct Answer:', st.session_state.answer_score[0])
@@ -169,8 +207,9 @@ if st.session_state.stage == 2:
 
     with st.expander('Show list of all possible answers'):
         # this is just to format it more nicely
-        for ca in correct_answers:
+        for ca in st.session_state.correct_answers:
             st.write('- '+ ca)
+
     st.button('Next Question', on_click=update_stage, args=(1,))
 
 if st.session_state.stage == 3:
